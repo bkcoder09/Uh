2767d865b834d68c894aa7bd397f5a81540d3409
bkcoder09 <80070457+bkcoder09@users.noreply.github.com>
2022-01-21 16:36:36 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 pic = "";
 status = "";
+objects = [];
 
 function preload(){
     pic = loadImage('dog_cat.jpg');
@@ -16,7 +17,7 @@ function setup(){
 function modelLoaded(){
     console.log(" The Model Is Loaded ");
     status = true;
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(pic, gotResult);
 }
 
 function gotResult(error, results){
@@ -25,21 +26,22 @@ function gotResult(error, results){
     }
     else{
         console.log(results);
+        objects = results;
     }
 }
 
 function draw(){
     image(pic, 0, 0, 700, 450);
 
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 315, 215);
-
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
+    if(status != ""){
+        for(i=0; i < objects.length; i++){
+        document.getElementById("status").innerHTML = "Status - Detecting Object =D";
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label+" "+percent+"%", objects[i].x+15, objects[i].y+15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
